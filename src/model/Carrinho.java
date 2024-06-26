c84695a24f0ecefef63a23797d8c2fd2ae546582
Davi Oliveira <davioliveira541@outlook.com>
2024-06-26 15:42:11 -0300

    Create Carrinho.java

---
+++ src/model/Carrinho.java
@@ -0,0 +1,74 @@
+package model;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+public class Carrinho
+{
+    private Map<Produto, Integer> produtosNoCarrinho;
+    private Estoque estoque;
+    
+    public Carrinho(Estoque estoque)
+    {
+        produtosNoCarrinho = new HashMap<>();
+        this.estoque = estoque;
+    }
+    
+    public void adicionarProduto(Produto produto, int quantidade) {
+        if (quantidade <= 0) {
+            throw new IllegalArgumentException("A quantidade deve ser maior que zero");
+        }
+        
+        int quantidadeEmEstoque = estoque.getQuantidadeProduto(produto);
+        int quantidadeNoCarrinho = produtosNoCarrinho.getOrDefault(produto, 0);
+        
+        if (quantidadeEmEstoque >= quantidadeNoCarrinho + quantidade)
+        {
+            produtosNoCarrinho.put(produto, quantidadeNoCarrinho + quantidade);
+        }
+        else
+        {
+            throw new IllegalArgumentException("Quantidade insuficiente em estoque");
+        }
+    }
+
+    // Remove uma quantidade específica de um produto do carrinho.
+    public void removerProduto(Produto produto, int quantidade)
+    {
+        if (produtosNoCarrinho.containsKey(produto))
+        {
+            int quantidadeAtual = produtosNoCarrinho.get(produto);
+            if (quantidadeAtual >= quantidade)
+            {
+                if (quantidadeAtual == quantidade)
+                {
+                    produtosNoCarrinho.remove(produto);
+                }
+                else
+                {
+                    produtosNoCarrinho.put(produto, quantidadeAtual - quantidade);
+                }
+            }
+            else
+            {
+                throw new IllegalArgumentException("Quantidade insuficiente no carrinho");
+            }
+        }
+        else
+        {
+            throw new IllegalArgumentException("Produto não encontrado no carrinho");
+        }
+    }
+
+    // Retorna a quantidade disponível de um produto específico no carrinho.
+    public int getQuantidadeProduto(Produto produto) {
+        return produtosNoCarrinho.getOrDefault(produto, 0);
+    }
+
+    // Retorna um Iterator para todos os produtos no carrinho.
+    public Iterator<Map.Entry<Produto, Integer>> listarProdutos() {
+        return produtosNoCarrinho.entrySet().iterator();
+    }
+}
+    
